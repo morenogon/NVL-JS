9ea4fbd8fe5980a16c66533d7168753c9c8295a5
morenogon <43233418+morenogon@users.noreply.github.com>
2020-04-27 15:29:19 +0200

    Create nvl_js_p9a.js

---
+++ NVL-JS-P9a/nvl_js_p9a.js
@@ -0,0 +1,4 @@
+var listaCompra = [["Manzanas",2],["Peras",3],["Plátanos",5]];
+alert("Longitud: " + listaCompra.length);
+alert("Longitud el primer elemento: " + listaCompra[0].length);
+alert("Segundo elemento del tercer alemento: " + listaCompra[2][1]);
