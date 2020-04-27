29202626172b47673bb3d8877ef786fe9cbb8140
morenogon <43233418+morenogon@users.noreply.github.com>
2020-04-27 12:38:16 +0200

    Create nvl-js-p8a.js

---
+++ NVL-JS-P8a/nvl-js-p8a.js
@@ -0,0 +1,7 @@
+var cadena = "Amigo";
+alert(cadena.toLowerCase());
+alert(cadena.replace("A","a"));
+
+var cadena_separada = cadena.slice(1,5);
+var nueva_cadena = "a" + cadena_separada;
+alert(nueva_cadena);
